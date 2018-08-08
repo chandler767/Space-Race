633e8c1ed11fbe62f6292693df16b90a7eb420d0
Chandler Mayo <chandler@pubnub.com>
2018-08-08 15:32:16 -0700

    Moved lobby functions to lobby.go.

--- main.go
+++ main.go
@@ -2,70 +2,14 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"bufio"
-	"strings"
-
-//	"github.com/gosuri/uiprogress"
-//	term "github.com/nsf/termbox-go"
+	pubnub "github.com/pubnub/go"
 )
 
-func newGame(channel string, username string) {
-	reader := bufio.NewReader(os.Stdin)
-	var (
-		err error
-		newChannel string
-		newUsername string 
-	)
-	
-	if (channel == "") { // Ask for channel name.
-		fmt.Print("Enter Channel Name: ")
-	} else {
-		fmt.Print("Enter Channel Name ("+channel+"): ")
-	}
-	newChannel, err = reader.ReadString('\n')
-	if err != nil {
-		panic(err)
-	}
-	newChannel = strings.Replace(newChannel, "\n", "", -1) // Convert CRLF to LF.
-	if newChannel != "" { // Use last channel name when the user does not provide a channel. 
-		channel = newChannel
-	}
-
-	if (username == "") { // Ask for username.
-		fmt.Print("Enter Username: ")
-	} else {
-		fmt.Print("Enter Username ("+username+"): ")
-	}
-	newUsername, err = reader.ReadString('\n')
-	if err != nil {
-		panic(err)
-	}
-	newUsername = strings.Replace(newUsername, "\n", "", -1) // Convert CRLF to LF.
-	if newUsername != "" {  // Use last username when the user does not provide a username. 
-		username = newUsername
-	}
-
-	if ((channel == "") || (username == "")) { // The user must have a channel and username.
-		fmt.Println("You Must Provide a Channel and Username! ")
-		newGame(channel, username) // Start over.
-		return
-	}
-
-
-
-}
-
 func main() {
-
-	var (
-		channel string
-		username string
-	)
-
+	config := pubnub.NewConfig()
+	config.SubscribeKey = "sub-c-8dd422dc-9a7e-11e8-b377-126307b646dc"
+	config.PublishKey = "pub-c-083cb2bb-500d-4bfc-910b-228bed5b5d71"
+	pn := pubnub.NewPubNub(config)
 	fmt.Println("Welcome to Space Race!")
-	
-	newGame(channel, username)
-
-	os.Exit(0)
+	newLobby("", "", pn) // Create a new game.
 }
