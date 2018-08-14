71de40477123fc15d1fabf74484b678ba1002568
Chandler Mayo <chandler@chandlermayo.com>
2018-08-14 11:16:30 -0700

    Replaced keys with placeholders.

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU()) // Use all available cpu cores.
 	config := pubnub.NewConfig()
-	config.SubscribeKey = "sub-c-8dd422dc-9a7e-11e8-b377-126307b646dc"
-	config.PublishKey = "pub-c-083cb2bb-500d-4bfc-910b-228bed5b5d71"
+	config.SubscribeKey = "sub-key"
+	config.PublishKey = "pub-key"
 	pn := pubnub.NewPubNub(config)
 	fmt.Println("Welcome to Space Race!")
 	newLobby("", "", pn) // Create lobby for a new game.
